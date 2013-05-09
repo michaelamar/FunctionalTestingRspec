fcaff781c489e72237f9dce6c4400793ebde78dc
Dan Garland <dan@dangarland.co.uk>
2013-05-09 13:57:22 -0400

    Basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecBlog::Application.config.secret_token = 'ce8df18b19baecf30c77e4e5eb7a7a33950c7ad3def5e9c8114ecce6c98833fa588bdd36e47b3394f3b055eb4cc246527866aefde65f234e906eab4015ad33bc'
